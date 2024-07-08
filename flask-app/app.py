a5e1d01517da48dc96163421735383c02bb8c4d8
ibs12 <89870752+ibs12@users.noreply.github.com>
2024-07-07 22:06:40 -0400

    added node js setup, and added content to database

--- flask-app/app.py
+++ flask-app/app.py
@@ -15,6 +15,17 @@ def get_db_connection():
     conn = psycopg2.connect(**config)
     return conn
 
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/flask', methods=['GET'])
+def index():
+    return "Flask server"
+
+
+
 @app.route('/api/articles', methods=['GET'])
 def get_articles():
     conn = get_db_connection()
@@ -40,5 +51,5 @@ def get_articles():
 
     return jsonify(articles_list)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(port=5000, debug=True)
